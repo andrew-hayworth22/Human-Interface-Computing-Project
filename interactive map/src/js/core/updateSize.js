ba5b29ebda01392e24e9739aed8f3ca89917c617
Andy Hayworth <mhaywort@kent.edu>
2022-04-19 11:55:16 -0400

    Removed Interactive map and fixed picture bug

--- interactive map/src/js/core/updateSize.js
+++
@@ -1,7 +0,0 @@
-export default function updateSize() {
-  this.width = this.container.offsetWidth
-  this.height = this.container.offsetHeight
-  this.resize()
-  this.canvas.setSize(this.width, this.height)
-  this.applyTransform()
-}
